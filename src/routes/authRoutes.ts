6e88707469abebd61f844cb2dd87e6a172068e41
dhiransapkota45 <dhiransapkota55@gmail.com>
2024-09-17 00:03:12 +0545

    feat: validate token endpoint added

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -3,6 +3,7 @@ import  express from 'express';
 import { asyncHandler } from "../utils/asyncHandler";
 import { Route } from "../types/route";
 import { AuthController } from "../controllers/authController";
+import { verifyToken } from "../middleware/verifyToken";
 
 export class AuthRoute implements Route {
   private authController: AuthController;
@@ -16,8 +17,7 @@ export class AuthRoute implements Route {
 
   setupRoutes() {
     this.router.post(`${this.path}/login`, asyncHandler(this.authController.login.bind(this.authController)));
-
     this.router.post(`${this.path}/register`, asyncHandler(this.authController.register.bind(this.authController)));
-
+    this.router.get(`${this.path}/validate-token`, verifyToken, asyncHandler(this.authController.validateToken.bind(this.authController)));
   }
 }
